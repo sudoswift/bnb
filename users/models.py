f74c1cab929db55829dce056ca7503d64dffd593
sudoswift <sudoswift@gmail.com>
2022-01-07 11:17:01 +0900

    update models.py

--- users/models.py
+++ users/models.py
@@ -5,4 +5,8 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
-    bio = models.TextField(default="")
+    '''Custom User Model''' #'''
+    
+    avatar = models.ImageField(null = True)
+    gender = models.CharField(max_length=10, null = True)
+    bio = models.TextField(null = True)
