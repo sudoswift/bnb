c946632567fbcc07f372b48560210e081d3701f2
sudoswift <sudoswift@gmail.com>
2022-01-11 14:24:41 +0900

    rooms.admin update

--- rooms/admin.py
+++ rooms/admin.py
@@ -5,6 +5,18 @@ from . import models
 class ItemAdmin(admin.ModelAdmin):
     pass
 
+@admin.register(models.Amenity)
+class AmenityAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.Facility)
+class FacilityAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.HouseRule)
+class HouseRule(admin.ModelAdmin):
+    pass
+
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
     pass
