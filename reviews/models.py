a34f1794734fd2a2399b6387cb49d36c29879bd7
sudoswift <sudoswift@gmail.com>
2022-01-13 12:19:51 +0900

    reviews model def __str__

--- reviews/models.py
+++ reviews/models.py
@@ -14,3 +14,8 @@ class Review(core_models.TimeStampedModel):
     value = models.IntegerField()
     user = models.ForeignKey("users.User", on_delete=models.CASCADE)
     room = models.ForeignKey("rooms.Room", on_delete=models.CASCADE)
+
+    # ForeignKey에서 가져온 모델의 Field의 모델의 Field 값들에 접근을 해서 그 값을 admin으로 가져올 수 있다!! 또한 그 Model의 Field가 또 ForeignKey라면(ForeignKey의 ForeignKey 값) 그 값 또한 가져올 수 있다!!
+    def __str__(self):
+        # Model 에서 두가지 이상의 field 값을 admin 패널에서 보여주고 싶다면 pyhton3의 f-string formate을 이용해 두가지 이상의 값을 가져올 수 있다람쥐.
+        return f'{self.review} - {self.room}'
